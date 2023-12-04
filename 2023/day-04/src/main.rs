71c17eeeb24c55dd6c5cf0ab919f48aef49772c1
Akinyele Cafe-Febrissy <me@akinyele.ca>
2023-12-04 00:13:14 -0500

    fix day 4 assets

--- 2023/day-04/src/main.rs
+++ 2023/day-04/src/main.rs
@@ -5,13 +5,13 @@ mod part2;
 
 fn main() {
     let instant = Instant::now();
-    let input = include_str!("./part1/input.txt");
+    let input = include_str!("./input.txt");
     let solution = part1::solution::execute(input);
 
     println!("[1] Result: {} ({:?})", solution, instant.elapsed());
 
     let instant = Instant::now();
-    let input = include_str!("./part1/input.txt");
+    let input = include_str!("./input.txt");
     let solution = part2::solution::execute(input);
 
     println!("[2] Result: {} ({:?})", solution, instant.elapsed());
@@ -23,14 +23,14 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        let input = include_str!("./part1/test.txt");
+        let input = include_str!("./test.txt");
         let solution = part1::solution::execute(input);
         assert_eq!(solution, 4361);
     }
 
     #[test]
     fn test_part_2() {
-        let input = include_str!("./part1/test.txt");
+        let input = include_str!("./test.txt");
         let solution = part2::solution::execute(input);
         assert_eq!(solution, 467835);
     }
